59aaff4c42cd6c0195cbc26598097a654e0307c3
Ralph Ribeiro <ralph_ribeiro@msn.com>
2021-01-26 18:57:28 -0300

    chapter 1

--- src/allocation/domain/model.py
+++ src/allocation/domain/model.py
@@ -15,8 +15,24 @@ class Batch:
         self.reference = ref
         self.sku = sku
         self.eta = eta
-        self.available_quantity = qty
+        self._purchased_quantity = qty
+        self._allocations: set[OrderLine] = set()
 
-    def allocate(self, line: OrderLine):
-        self.available_quantity -= line.qty
-        
+    def allocate(self, line: OrderLine) -> None:
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine) -> None:
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    def can_allocate(self, line: OrderLine) -> bool:
+        return line.qty <= self.available_quantity and line.sku == self.sku
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
