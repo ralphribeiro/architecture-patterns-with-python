70198edac94f15f5a8165b568b73ef575fefc6d0
Ralph Ribeiro <ralph_ribeiro@msn.com>
2021-02-08 12:05:29 -0300

    chapter 10

--- src/allocation/service_layer/messagebus.py
+++ src/allocation/service_layer/messagebus.py
@@ -1,26 +1,80 @@
 from __future__ import annotations
-from typing import Callable, TYPE_CHECKING
+from logging import Logger
+from tenacity import Retrying, RetryError, stop_after_attempt, wait_exponential
+from typing import Callable, TYPE_CHECKING, Union
 
-from src.allocation.domain import events
+from src.allocation.domain import commands, events
 from . import handlers
 if TYPE_CHECKING:
     from . import unit_of_work
 
+logger = Logger(__name__)
 
-def handle(event: events.Event, uow: unit_of_work.AbstractUnitOfWork):
+Message = Union[commands.Command, events.Event]
+
+
+def handle(message: Message, uow: unit_of_work.AbstractUnitOfWork):
     results = []
-    queue = [event]
+    queue = [message]
     while queue:
-        event = queue.pop(0)
-        for handler in HANDLERS[type(event)]:
-            results.append(handler(event, uow=uow))
-            queue.extend(uow.collect_new_events())
+        message = queue.pop(0)
+        if isinstance(message, events.Event):
+            handle_event(message, queue, uow)
+        elif isinstance(message, commands.Command):
+            cmd_result = handle_command(message, queue, uow)
+            results.append(cmd_result)
+        else:
+            raise Exception(f'{message} was not an Event or Command')
     return results
 
 
-HANDLERS: dict[type[events.Event], list[Callable]] = {
-    events.BatchCreated: [handlers.add_batch],
-    events.BatchQuantityChanged: [handlers.change_batch_quantity],
-    events.AllocationRequired: [handlers.allocate],
+def handle_event(
+    event: events.Event,
+    queue: list[Message],
+    uow: unit_of_work.AbstractUnitOfWork
+):
+    for handler in EVENT_HANDLERS[type(event)]:
+        try:
+            for attempt in Retrying(
+                stop=stop_after_attempt(3),
+                wait=wait_exponential()
+            ):
+                with attempt:
+                    logger.debug(
+                        'handling event %s with handler %s', event, handler)
+                    handler(event, uow)
+                    queue.extend(uow.collect_new_events())
+        except RetryError as retry_failure:
+            logger.error(
+                'Failed to handle event %s times, giving up!',
+                retry_failure.last_attempt.attempt_number
+            )
+            continue
+
+
+def handle_command(
+    command: commands.Command,
+    queue: list[Message],
+    uow: unit_of_work.AbstractUnitOfWork
+):
+    logger.debug('handling command %s ', command)
+    try:
+        handler = COMMAND_HANDLERS[type(command)]
+        result = handler(command, uow)
+        queue.extend(uow.collect_new_events())
+        return result
+    except Exception:
+        logger.exception('Exception handling command %s', command)
+        raise
+
+
+EVENT_HANDLERS: dict[type[events.Event], list[Callable]] = {
     events.OutOfStock: [handlers.send_out_of_stock_notification],
-} 
+}
+
+
+COMMAND_HANDLERS: dict[type[commands.Command], Callable] = {
+    commands.Allocate: handlers.allocate,
+    commands.CreateBatch: handlers.add_batch,
+    commands.ChangeBatchQuantity: handlers.change_batch_quantity,
+}
